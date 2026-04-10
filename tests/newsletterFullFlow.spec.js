c50cb46da440b62f58e5d08d7c5213dddd0c845c
vanshaj.yadav <vanshajsgyadav@gmail.com>
2026-04-10 14:22:07 +0530

    Updated File

--- tests/newsletterFullFlow.spec.js
+++ tests/newsletterFullFlow.spec.js
@@ -12,39 +12,60 @@ test('Newsletter Full Flow - End to End', async ({ page }) => {
   const list = new NewsletterListPage(page);
   const details = new NewsletterDetailsPage(page);
 
-
+  
   await login.goto();
   await login.login('admin@yourstore.com', 'admin');
 
+  //Assert
+  await expect(page).toHaveURL(/admin/i);
+
+ 
   await nav.navigateToSubscribers();
-  expect(nav.isPageLoaded()).toBeTruthy();
+  await expect(nav.pageTitle).toBeVisible();
+  await expect(nav.pageTitle).toHaveText(/Newsletter subscribers/i);
+
+  // NEGATIVE CASE: Empty Email
+  await list.clickAddNew();
+  await details.clickSave();
+
+  const errorMsg = page.locator('.field-validation-error');
+
+  await expect(errorMsg).toBeVisible();
+  await expect(errorMsg).toContainText(/required/i);
+
+  
+  await nav.navigateToSubscribers();
 
   await list.clickAddNew();
 
   const email = `test${Date.now()}@mail.com`;
 
   await details.enterEmail(email);
+  await details.setActiveRadio(true);
   await details.clickSave();
 
-  expect(await details.isSuccessMessageDisplayed()).toBeTruthy();
+ 
+  await expect(details.successMessage).toBeVisible();
+  await expect(details.successMessage).toContainText(/success/i);
+
 
   await nav.navigateToSubscribers();
   await list.search(email);
 
-  expect(await list.isGridVisible()).toBeTruthy();
+  await expect(list.grid).toBeVisible();
+  await expect(list.grid).toContainText(email);
 
+  
   await list.clickEdit();
-
 
   const isActive = await details.isActiveRadioChecked();
 
-  console.log("Current Active State:", isActive);
-
   await details.setActiveRadio(!isActive);
-
   await details.clickSave();
 
-  expect(await details.isSuccessMessageDisplayed()).toBeTruthy();
+  
+  await expect(details.successMessage).toBeVisible();
+  await expect(details.successMessage).toContainText(/updated|success/i);
 
   await nav.navigateToSubscribers();
   await list.search(email);
@@ -52,17 +73,23 @@ test('Newsletter Full Flow - End to End', async ({ page }) => {
 
   await details.deleteSubscriber();
 
-  expect(await details.isSuccessMessageDisplayed()).toBeTruthy();
 
+  await expect(details.successMessage).toBeVisible();
+  await expect(details.successMessage).toContainText(/deleted|success/i);
+
+  
   await nav.navigateToSubscribers();
 
-  await list.importCSV('test-data/newsletter.csv');
+  const path = require('path');
+  const filePath = path.join(__dirname, '../test-data/newsletter.csv');
 
-  expect(await list.isSuccessMessageDisplayed()).toBeTruthy();
+  await list.importCSV(filePath);
 
+ 
+  await expect(list.successMessage).toBeVisible();
+  await expect(list.successMessage).toContainText(/imported|success/i);
+
+  
   await list.exportCSV();
 
 });
-
-
-
