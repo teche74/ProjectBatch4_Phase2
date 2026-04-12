1fba5c866e7b4ba672c2cf62ca925fd163b97e41
maahi240903 <maahiladukar@gmail.com>
2026-04-12 19:52:32 +0530

    Update discountDetailsPage.js

--- pages/discountDetailsPage.js
+++ pages/discountDetailsPage.js
@@ -1,31 +1,49 @@
 const BasePage = require('./basePage');
 
 class DiscountDetailsPage extends BasePage {
-    constructor(page) {
-        super(page);
-        this.nameInput = page.locator('#Name');
-        this.usePercentageCheckbox = page.locator('#UsePercentage');
-        this.discountPercentage = page.locator('#DiscountPercentage');
-        this.saveBtn = page.locator('button[name="save"]');
-        this.deleteBtn = page.locator('#discount-delete');
-        this.confirmDeleteBtn = page.locator('button.btn-danger[type="submit"]');
-        this.successAlert = page.locator('.alert-success');
+  constructor(page) {
+    super(page);
+
+    this.nameField = page.locator('#Name');
+    this.usePercentageCheckbox = page.locator('#UsePercentage');
+    this.discountPercentage = page.locator('#DiscountPercentage');
+
+    this.saveBtn = page.locator('button[name="save"]');
+
+    this.deleteBtn = page.locator('#discount-delete');
+    this.confirmDeleteBtn = page.locator('.modal-dialog .btn-danger');
+
+    this.successMessage = page.locator('.alert-success');
+  }
+
+  async enterName(name) {
+    await this.fill(this.nameField, name);
+  }
+
+  async setPercentage(value) {
+    const isChecked = await this.usePercentageCheckbox.isChecked();
+
+    if (!isChecked) {
+      await this.usePercentageCheckbox.check();
     }
 
-    async fillDiscountDetails(name, percentage) {
-        await this.nameInput.fill(name);
-        const isChecked = await this.usePercentageCheckbox.isChecked();
-        if (!isChecked) await this.usePercentageCheckbox.check();
-        await this.discountPercentage.fill(percentage.toString());
-    }
+    await this.fill(this.discountPercentage, value.toString());
+  }
 
-    async save() {
-        await this.saveBtn.click();
-    }
+  async clickSave() {
+    await this.click(this.saveBtn);
+  }
 
-    async delete() {
-        await this.deleteBtn.click();
-        await this.confirmDeleteBtn.click();
-    }
+  async deleteDiscount() {
+    await this.click(this.deleteBtn);
+
+    await this.confirmDeleteBtn.waitFor({ state: 'visible' });
+    await this.click(this.confirmDeleteBtn);
+  }
+
+  async isSuccessMessageDisplayed() {
+    return await this.successMessage.isVisible();
+  }
 }
+
 module.exports = DiscountDetailsPage;
